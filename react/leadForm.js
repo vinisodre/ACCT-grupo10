332683d28247d5ba328fc658d85b7abe66d4c5ce
adriana <adiefco@gmail.com>
2021-08-30 23:29:19 -0300

    4th

--- react/leadForm.js
+++ react/leadForm.js
@@ -1,50 +1,70 @@
-import React, { useState } from "react";
-import {Container, Content, Input, Label, Button} from './styles'
-import Axios from "axios";
+import React, { useState } from 'react';
+import { Container, Content, Input, Label, Button } from './styles';
+import Axios from 'axios';
 
 const leadForm = () => {
-    const url = "https://4wnkgl4pyd.execute-api.sa-east-1.amazonaws.com/prod/leads"
-    const [data, setData] =  useState({
-        name:"",
-        email:"",
-        phone:"",
-        cpf:""
-    })
+  const url =
+    'https://4wnkgl4pyd.execute-api.sa-east-1.amazonaws.com/prod/leads';
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    cpf: '',
+  });
 
-    function submit(e){
-        e.preventDefault();
-        Axios.post(url, {
-            nome: data.name,
-            email: data.email,
-            telefone: parseInt(data.phone),
-            cpf: parseInt(data.cpf)
-        })
-        .then(res=>{
-            console.log(res.data)
-        })
-    }
+  function submit(e) {
+    e.preventDefault();
+    Axios.post(url, {
+      nome: data.name,
+      email: data.email,
+      telefone: parseInt(data.phone),
+      cpf: parseInt(data.cpf),
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }
 
-    function handle(e){
-        const newdata ={...data}
-        newdata[e.target.id] = e.target.value
-        setData(newdata)
-    }
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+  }
 
-    return(
-        <Container>
-            <Content onSubmit= {(e)=> submit(e)}>
-                <Label htmlFor="name">Nome</Label>
-                <Input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="name" type="text"></Input>
-                <Label htmlFor="email">Email</Label>
-                <Input onChange={(e)=>handle(e)} id="email" value={data.email} placeholder="email" type="email"></Input>
-                <Label htmlFor="phone">Telefone</Label>
-                <Input onChange={(e)=>handle(e)} id="phone" value={data.phone} placeholder="phone" type="number"></Input>
-                <Label htmlFor="cpf">CPF</Label>
-                <Input onChange={(e)=>handle(e)} id="cpf" value={data.cpf} placeholder="cpf" type="number"></Input>
-                <Button>Enviar</Button>
-            </Content>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Content onSubmit={(e) => submit(e)}>
+        <Label htmlFor="name">Nome*</Label>
+        <Input
+          onChange={(e) => handle(e)}
+          id="name"
+          value={data.name}
+          type="text"
+        ></Input>
+        <Label htmlFor="email">Email*</Label>
+        <Input
+          onChange={(e) => handle(e)}
+          id="email"
+          value={data.email}
+          type="email"
+        ></Input>
+        <Label htmlFor="phone">Telefone*</Label>
+        <Input
+          onChange={(e) => handle(e)}
+          id="phone"
+          value={data.phone}
+          type="number"
+        ></Input>
+        <Label htmlFor="cpf">CPF*</Label>
+        <Input
+          onChange={(e) => handle(e)}
+          id="cpf"
+          value={data.cpf}
+          type="number"
+        ></Input>
+        <Button>Enviar</Button>
+      </Content>
+    </Container>
+  );
+};
 
-export default leadForm
+export default leadForm;
